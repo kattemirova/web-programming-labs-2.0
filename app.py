cbe513ad5eb33d7b27e17c0f4949208976133611
Темирова Пахомова <katya.temirova.2000@mail.ru>
2023-09-23 17:02:05 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -200,3 +200,7 @@ def example():
     ]
 
     return render_template('example.html', name=name, number=number, group=group, kurs=kurs, fruits=fruits, books=books)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
