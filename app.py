d91497290c4ad41582dc9ec22d0dc3f926645a4a
Темирова Пахомова <katya.temirova.2000@mail.ru>
2023-09-23 16:44:04 +0700

    Цикл в шаблоне+Книги

--- app.py
+++ app.py
@@ -177,8 +177,26 @@ def vostok():
 
 @app.route('/lab2/example')
 def example():
-    name = 'Темирова Екатерина, Пахомова Валерия'
-    number = '2'
-    group = 'ФБИ-11'
-    kurs = '3 курс'
-    return render_template('example.html', name=name, number=number, group=group, kurs=kurs)
+    name, number,  group, kurs = 'Темирова Екатерина, Пахомова Валерия', 2, 'ФБИ-11', '3 курс'
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    
+    books = [
+        {'avtor': 'Кирилл Кутузов', 'name': 'Бесобой', 'zanr': 'Фантастика', 'str': 240},
+        {'avtor': 'Борис Акунин', 'name': 'Азазель', 'zanr': 'Детектив', 'str': 272},
+        {'avtor': 'Борис Акунин', 'name': 'Турецкий гамбит', 'zanr': 'Военный/Приключения', 'str': 208},
+        {'avtor': 'Борис Акунин', 'name': 'Левиафан', 'zanr': 'Детектив', 'str': 624},
+        {'avtor': 'Дженнифер Линн Барнс', 'name': 'Последний гамбит', 'zanr': 'Триллер', 'str': 416},
+        {'avtor': 'Николай Калиниченко', 'name': 'Королевский гамбит', 'zanr': 'Спорт/Самооборона', 'str': 416},
+        {'avtor': 'Элизабет Фримантл', 'name': 'Гамбит королевы', 'zanr': 'Исторические романы', 'str': 480},
+        {'avtor': 'Мамлеев Юрий', 'name': 'Московский гамбит', 'zanr': 'Роман', 'str': 296},
+        {'avtor': 'Евгений Руднев', 'name': 'Сибирский гамбит', 'zanr': 'Биография', 'str': 288},
+        {'avtor': 'Ерофей Трофимов', 'name': 'Лесной гамбит', 'zanr': 'Роман', 'str': 310},
+    ]
+
+    return render_template('example.html', name=name, number=number, group=group, kurs=kurs, fruits=fruits, books=books)
