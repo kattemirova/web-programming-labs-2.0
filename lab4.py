796b60fbb44f15f5e02b9f732d2cea81555e9b0a
Темирова Пахомова <katya.temirova.2000@mail.ru>
2023-11-06 18:48:51 +0700

    Куки

--- lab4.py
+++ lab4.py
@@ -85,3 +85,15 @@ def zerno():
         return render_template('zerno.html', error=error, weight=weight)
 
     return render_template('zerno.html', sk=sk, price=price, zerno=zerno, weight=weight)
+
+@lab4.route('/lab4/cookies', methods = ['GET', 'POST'])
+def cookies():
+    if request.method == 'GET':
+        return render_template('cookies.html')
+    
+    color = request.form.get('color')
+    headers = {
+        'Set-Cookie': 'color=' + color + '; path=/',
+        'Location': '/lab4/cookies'
+    }
+    return '', 303, headers
