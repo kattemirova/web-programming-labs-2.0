fdefa19c7c8fa12f7371d0bfcdc76951d3a18758
Темирова Пахомова <katya.temirova.2000@mail.ru>
2023-11-23 09:24:03 +0700

    Правки

--- lab5.py
+++ lab5.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, request, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Blueprint, render_template, request, redirect, session
+from flask_session import Session
 import psycopg2
+
 lab5 = Blueprint('lab5', __name__)
 
 def dbConnect():
@@ -18,7 +21,6 @@ def dbClose(cursor, connection):
 @lab5.route("/lab5")
 def main():
     username = "Anon"
-    
     return render_template ('glav.html', username=username)
 
 @lab5.route('/lab5/users')
@@ -31,4 +33,131 @@ def users():
 
 
     return render_template('users.html', users=users)
-    
+
+@lab5.route('/lab5/register', methods=["GET", "POST"])
+def registerPage():
+    errors = []
+
+    if request.method == "GET":
+        return render_template("register.html", errors=errors)
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not (username or password):
+        errors.append("Пожалуйста, заполните все поля")
+        print(errors)
+        return render_template("register.html", errors=errors)
+
+    hashPassword = generate_password_hash(password)
+    conn = dbConnect()
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT username FROM users WHERE username = '{username}';")
+
+    if cur.fetchone() is not None:
+        errors.append("Пользователь с данным именем уже существует")
+
+        conn.close()
+        cur.close()
+        
+        return render_template("register.html", errors=errors)
+
+    cur.execute(f"INSERT INTO users (username, password) VALUES ('{username}','{hashPassword}');")
+
+    conn.commit()
+    conn.close()
+    cur.close()
+
+    return redirect("/lab5/login")
+
+@lab5.route('/lab5/login', methods = ["GET", "POST"])
+def login():
+    errors = []
+
+    if request.method == "GET":
+        return render_template("login.html", errors=errors)
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not (username or password):
+        errors.append('Пожалуйста, заполните все поля')
+        return render_template("login.html", errors=errors)
+
+    conn = dbConnect()
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT id, password FROM users WHERE username = '{username}'")
+
+    result = cur.fetchone()
+
+    if result is None:
+        errors.append("Неправильный логин или пароль")
+        dbClose(cur, conn)
+        return render_template("login.html", errors=errors)
+
+    userID, hashPassword = result
+
+    if check_password_hash(hashPassword, password):
+        session['id'] = userID
+        session['username'] = username
+        dbClose(cur, conn)
+        return render_template("glav.html", username=username)
+
+    else:
+        errors.append("Неправильный логин или пароль")
+        return render_template("login.html", errors=errors)
+
+@lab5.route("/lab5/new_article", methods = ["GET", "POST"])
+def createArticle():
+    errors =[]
+
+    userID = session.get("id")
+
+    if userID is not None:
+        if request.method == "GET":
+            return render_template("new_article.html")
+        
+        if request.method == "POST":
+            text_article = request.form.get("text_article")
+            title = request.form.get("title_article")
+
+            if len(text_article) == 0:
+                errors.append('Заполните текст')
+                return render_template("new_article.html", errors=errors)
+
+            conn = dbConnect()
+            cur = conn.cursor()
+
+            cur.execute(f"INSERT INTO articles(user_id, title, article_text) VALUES ('{userID}', '{title}', '{text_article}') RETURNING id")
+
+            new_article_id = cur.fetchone()[0]
+            conn.commit()
+
+            dbClose(cur, conn)
+
+            return redirect(f"/lab5/articles/{new_article_id}")
+
+    return redirect("/lab5/login")
+
+@lab5.route("/lab5/articles/<int:article_id>")
+def getArticle(article_id):
+    userID = session.get("id")
+
+    if userID is not None:
+        conn = dbConnect()
+        cur = conn. cursor()
+
+        cur.execute(f"SELECT title, article_text FROM articles WHERE id = %s and user_id = %s", (article_id, userID))
+
+        articleBody = cur.fetchone()
+
+        dbClose(cur, conn)
+
+        if articleBody is None:
+            return "Not found!"
+
+        text = articleBody[1].splitlines()
+
+        return render_template("articles.html", article_text=text, article_title=articleBody[0], username=session.get("username"))
