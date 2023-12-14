f875c4471432be8fe06f4302b88c743cdb48b9db
Темирова Екатерина <katya.temirova.2000@mail.ru>
2023-12-14 10:30:41 +0700

    Удаление выбранного курса по REST

--- lab8.py
+++ lab8.py
@@ -6,3 +6,29 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
 def main():
     return render_template('lab8/index.html')
+
+courses = [
+    {"name": "c++", "videos": 3, "price": 3000},
+    {"name": "basic", "videos": 30, "price": 0},
+    {"name": "c#", "videos": 8}
+]
+
+
+@lab8.route('/lab8/api/courses/', methods=['GET'])
+def get_courses():
+    return courses
+
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
+def get_course(course_num):
+    if course_num >= len(courses):
+        return 'Такого курса не существует'
+    return courses[course_num]
+
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['DELETE'])
+def del_course(course_num):
+    if course_num >= len(courses):
+        return 'Такого курса не существует'
+    del courses[course_num]
+    return '', 204
