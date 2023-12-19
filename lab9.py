10c616bc4e53db5c91fd0617bcc63d7f21c5ecd6
Темирова Екатерина <katya.temirova.2000@mail.ru>
2023-12-19 22:05:20 +0700

    Обработчик ошибки 500

--- lab9.py
+++ lab9.py
@@ -9,3 +9,11 @@ def main():
 @lab9.app_errorhandler(404)
 def not_found(e):
     return render_template('lab9/404.html'), 404
+
+@lab9.app_errorhandler(500)
+def error(e):
+    return render_template('lab9/500.html'), 500
+
+@lab9.route('/lab9/500')
+def server_error():
+    return render_template('lab9/500.html'), 500
